eb5fa64e117bdb965ab40cc6ef43ad9247ddd4e7
Jose Fernandes <zemiranda04@gmail.com>
2025-06-21 18:49:42 +0100

    remover ficheiros desnecessarios

--- poseScript.py
+++
@@ -1,5 +0,0 @@
-#pip install mediapipe opencv-python python-osc
-#antes de correr qualquer coisa para ativar o virtual environment de python3.11
-#no cmd
-#cd C:\Users\zemir\Desktop\YogaKinetic
-#.\venv\Scripts\activate.bat
